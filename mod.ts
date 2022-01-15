e2f281f94fc3729112ab01c77e4aad6bf22be3fc
yskszk63 <yskszk63@gmail.com>
2022-01-15 21:26:21 +0900

    Moved the symbol resolution back.

--- mod.ts
+++ mod.ts
@@ -1,5 +1,3 @@
-// TODO errno is TLS?
-
 const libdl = Deno.dlopen('libdl.so', {
   "dlopen": {
     parameters: ["pointer", "i32"],
@@ -16,7 +14,9 @@ const libdl = Deno.dlopen('libdl.so', {
 // #define RTLD_LAZY	0x00001	/* Lazy function call binding.  */
 const RTLD_LAZY: number = 0x00001;
 
-const errnoHandle = (function(): Deno.UnsafePointerView {
+const errnoName = Uint8Array.of(0x65, 0x72, 0x72, 0x6e, 0x6f, 0x00); // errno\0
+
+const handle = (function(): Deno.UnsafePointer {
   const nul = new Deno.UnsafePointer(0n);
   const handle = libdl.symbols.dlopen(nul, RTLD_LAZY);
   if (!(handle instanceof Deno.UnsafePointer)) {
@@ -25,18 +25,17 @@ const errnoHandle = (function(): Deno.UnsafePointerView {
   if (handle.value === 0n) {
     throw new Error();
   }
+  return handle;
+})();
 
-  const errno = Uint8Array.of(0x65, 0x72, 0x72, 0x6e, 0x6f, 0x00); // errno\0
-  const herrno = libdl.symbols.dlsym(handle, errno);
+export default function errno(): number {
+  const herrno = libdl.symbols.dlsym(handle, errnoName);
   if (!(herrno instanceof Deno.UnsafePointer)) {
     throw new Error();
   }
   if (herrno.value === 0n) {
     throw new Error();
   }
-  return new Deno.UnsafePointerView(herrno);
-})();
-
-export default function errno(): number {
-  return errnoHandle.getInt32();
+  const ptr = new Deno.UnsafePointerView(herrno);
+  return ptr.getInt32();
 }
